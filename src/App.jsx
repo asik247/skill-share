3d822508075e60628977b9e11f1c34c76c59763d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-10 09:40:42 +0600

    router and root folder create

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1 className='font-bold text-center roboto'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, maxime.</h1>
+     
     </div>
   );
 };
