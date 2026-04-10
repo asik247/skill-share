11f4101a56125ca3e235f290ca11eb4ff6188b62
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-10 23:00:08 +0600

    home page design started

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,12 +2,16 @@ import { createBrowserRouter } from "react-router";
 import Roots from "../Root/Roots";
 import Home from "../Pages/Home";
 import LogIn from "../Components/LogIn";
+import Profile from "../Pages/Profile";
+import Contact from "../Pages/Contact";
 const router = createBrowserRouter([
     {
         path:'/',
         Component:Roots,
         children:[
             {index:true,Component:Home},
+            {path:'profile',Component:Profile},
+            {path:'contact',Component:Contact},
             {path:'login',Component:LogIn}
         ]
     }
